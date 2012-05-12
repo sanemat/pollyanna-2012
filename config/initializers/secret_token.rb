07c9517dfcc2df7427aa5b501597f1330b4b1e92
sanemat <o.gata.ken@gmail.com>
2012-05-12 17:53:49 +0900

    Create app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pollyanna::Application.config.secret_token = 'f79fc129e8894b0bf01463b4bf15370c12037873c3cb667ab5e5f9637d4175488e346149484584a5c5b4cdcc0ecb1400fdc4aa85b620daa32e83683c879f6f0f'
