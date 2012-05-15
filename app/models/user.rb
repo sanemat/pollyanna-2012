c7134cda287fc2d9e35b957dec597f4dafe1574d
sanemat <o.gata.ken@gmail.com>
2012-05-15 13:29:20 +0900

    Add name for mass assignment.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ActiveRecord::Base
          :confirmable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :confirmed_at
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :confirmed_at, :name
   # attr_accessible :title, :body
 end
